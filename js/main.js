ac3dbef3849da168ce8fa38c1682649969df88ea
Brendan Kormos <brendaaank@gmail.com>
2023-11-30 23:03:40 +0000

    view entries styling change

--- js/main.js
+++ js/main.js
@@ -1,21 +1,20 @@
 // https://lldev.thespacedevs.com/2.2.0/launcher/?limit=20
 // https://lldev.thespacedevs.com/2.2.0/launcher/ID/
-const $listEntry = document.querySelector('.list-entry')
+const $listEntry = document.querySelector('.list-entry');
 const $listContainer = document.querySelector('.masonry-holder');
 
-import entries20 from "./Entries_20.json" assert { type: 'json'}
+import entries20 from './Entries_20.json' assert { type: 'json' };
 
 console.log(entries20);
-function renderListEntry(entry){
-  const $clone = $listEntry.cloneNode(true)
-  $clone.classList.remove('hidden')
-  $clone.querySelector('img').src = entry.image_url
-  return $clone
+function renderListEntry(entry) {
+  const $clone = $listEntry.cloneNode(true);
+  $clone.classList.remove('hidden');
+  $clone.querySelector('img').src = entry.image_url;
+  return $clone;
 }
 
-for (let i = 0; i < entries20.results.length; i++){
-  const dataEntry = entries20.results[i]
-  const $entry = renderListEntry(dataEntry)
-  console.dir($listContainer)
-  $listContainer.append($entry)
+for (let i = 0; i < entries20.results.length; i++) {
+  const dataEntry = entries20.results[i];
+  const $entry = renderListEntry(dataEntry);
+  $listContainer.append($entry);
 }
