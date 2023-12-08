dfdbf65fc13b79a978eee83fdc8140d2d9248f1e
Brendan Kormos <brendaaank@gmail.com>
2023-12-08 14:31:46 +0000

    wipes data if doesn't have newest feature in data

--- js/data.js
+++ js/data.js
@@ -10,18 +10,24 @@ let data = {
   scrollPositions: {
     'home-container': 0,
     'single-entry-container': 0,
-    'saves-container': 0
+    'saves-container': 0,
   },
 };
 
 function saveScrollPosition() {
   const verticalScrollPosition = window.scrollY || window.pageYOffset;
-  data.scrollPositions[data.view] = verticalScrollPosition
+  data.scrollPositions[data.view] = verticalScrollPosition;
 }
 const oldData = localStorage.getItem(LOCAL_STORAGE_KEY);
-if (oldData) data = JSON.parse(oldData);
+if (oldData) {
+  const parsedOldData = JSON.parse(oldData);
+  if (parsedOldData.scrollPositions || parsedOldData.scrollPositions['saves-container'] !== undefined) {
+    //if it doesn't have newest feature, wipe data
+    data = parsedOldData;
+  }
+}
 
 window.addEventListener('unload', function (event) {
-  saveScrollPosition()
+  saveScrollPosition();
   localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(data));
 });
